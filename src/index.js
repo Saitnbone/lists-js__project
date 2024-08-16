1d5b658b22b95ca836db7f59587b92480d079c90
Aleksandr Golodin <golodinaleksandr@gmail.com>
2024-08-16 16:07:29 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 // Импорты
+// imports
 import { onSubmit, checkInput } from "./scripts/form.js";
 import { switchButton } from "./scripts/switchButtons.js";
 import { loadFromLocalStorage } from "./scripts/localstorege.js";
 
-// Определите все узлы
+//  DOM-узлы
+// Dom-nodes
 const switchButtons = document.querySelectorAll(".switch-block__element");
 const formTodo = document.querySelector(".form-section");
 const formInput = formTodo.querySelector(".form-todo__input");
 const formTodoButton = formTodo.querySelector(".form-todo__button");
 
 // Слушатели событий для формы
+// Event listeners for the form
 formInput.addEventListener("input", (event) =>
   checkInput(event, formTodoButton)
 );
@@ -19,6 +22,7 @@ formTodo.addEventListener("submit", (event) =>
 );
 
 // Слушатели событий для переключателей
+// Event listeners for radio buttons
 switchButtons.forEach((button) => {
   button.addEventListener("click", (event) =>
     switchButton(switchButtons, event)
@@ -26,11 +30,14 @@ switchButtons.forEach((button) => {
 });
 
 // Функция для вызова рендеринга всех элементов списков
+// Function to call rendering of all list elements
 document.addEventListener("DOMContentLoaded", () => {
   // Очистка текущих элементов в DOM
+  // Clear current elements in DOM
   document.querySelector(".active-cases").innerHTML = "";
   document.querySelector(".completed-cases").innerHTML = "";
 
   // Загрузка данных из localStorage
+  // Load data from localStorage
   loadFromLocalStorage();
 });
